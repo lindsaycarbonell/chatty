9b55440253f22efa0a7c86a7cd9d1586f076fa5f
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2017-06-27 12:34:18 -0400

    connected to heroku, got test for textrazor working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chatty
 In which we build a kind of functioning fancy chatbot
+
+## Outside Code
+- [Text Razor SDK](https://github.com/wzbg/textrazor) by wzbg
---
+++ razor.js
@@ -0,0 +1,25 @@
+const TextRazor = require('textrazor');
+const textRazor = new TextRazor('98d2b4160bce259e9e5ede577635f209f4acd81d5ca8a360ead4779f');
+const content = 'Have the Wilmington city leaders considered building a minor league baseball stadium on the Convention Ctr property?';
+const options = { extractors: 'entities' };
+
+//entities are tags
+//entities[0].entityEnglishId
+
+// entities, topics, words, phrases,
+// dependency-trees, relations, entailments, senses, spelling
+
+textRazor.exec(content, options)
+  .then(res => parseResults(res))
+  .catch(err => console.error(err));
+
+function parseResults(res){
+  const entities = res.response.entities;
+  var tags = [];
+  // console.log(JSON.stringify(res, null, 4))
+
+  for (i in entities){
+    tags.push(entities[i].entityId);
+  }
+  console.log(tags);
+}
